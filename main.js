da27428c5742303d9b6018253a7933fa1bf0f53e
ZuzuStack <ridwanyamada@gmail.com>
2023-02-28 17:05:00 +0700

    yah begitulah

--- main.js
+++ main.js
@@ -3,19 +3,53 @@ import { Server } from "socket.io";
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
-  cors : {
+  cors: {
     origin: "*",
-  }
+  },
+});
+
+let user = [];
+
+io.of("/login").on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("login", (username) => {
+    if (!user.find((e) => e.name == username)) {
+      socket.emit("login", {
+        id: socket.id,
+        status: "Berhasil Login",
+      });
+      user.push({
+        name: username,
+        id: socket.id,
+      });
+    } else {
+      socket.emit("login", {
+        status: "Nama Sudah Dipakai",
+      });
+    }
+  });
 });
 
 io.on("connection", (socket) => {
-  const request =  socket.handshake.query
+  const request = socket.handshake.query;
 
   socket.on(`roomId:${request.room}`, (data) => {
     console.log(data);
-    socket.broadcast.emit(`roomId:${request.room}`, data);
-  })
+    socket.broadcast.emit(`roomId:${request.room}`, {
+      username: data.username,
+      message: data.message,
+      timestamp: new Date(),
+    });
+  });
 
+  socket.on("disconnect", () => {
+    user.forEach((e, i) => {
+      e.id === request.id ? user.splice(i, 1) : "";
+    });
+
+    console.log(user);
+  });
 });
 
 httpServer.listen(3000);
